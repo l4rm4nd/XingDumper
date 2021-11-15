bf667ee24d1fae0f27fe86aa99e8c67eff73226c
L4RM4ND <laurent.vetter@protonmail.com>
2021-11-15 13:24:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# XingDumper
+Python 3 script to dump company employees from XING API. Perfect OSINT tool ;-)
+
+The CSV results contain firstname, lastname, position, gender, location and a user's profile link.
+
+## How-To
+1. Sign into www.xing.com and retrieve your ``login`` cookie value e.g. via developer tools
+2. Specify your cookie value in the python script's variable ``LOGIN_COOKIE`` or via the CLI flag ``--cookie``
+3. Browse your company on XING and note the url
+4. Install requirements via ``pip install -r requirements.txt``
+5. Run the Python script and enjoy results
+
+## Usage
+````
+usage: xingdumper.py [-h] --url <xing-url> [--count <number>] [--cookie <cookie>] [--full] [--quiet]
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --url <xing-url>   A XING company url - https://xing.com/pages/<company>
+  --count <number>   Amount of employees to extract - max. 2999
+  --cookie <cookie>  XING 'login' cookie for authentication
+  --full             Dump additional contact details (slow) - email, phone, fax, mobile
+  --quiet            Show employee results only
+````
+
+## Examples
+
+Dumping all Audi employees from XING API (max. 3000) into outfile using `--quiet` mode:
+````
+python3 xingdumper.py --url https://www.xing.com/pages/audiag --quiet > audi_employees.out
+````
+Dumping 10 Apple employees from XING API with additional contact details as terminal output:
+````
+python3 xingdumper.py --url https://www.xing.com/pages/appleretaildeutschlandgmbh --count 10 --full
+````
+**Note**: Additional contact details are most often empty. We Germans take privacy seriously! Futher, the details may only be accessible if you belong to the contact list of the crawled employee. Kinda unlikely. 
+
+## Results
+
+The script will return employee data as semi-colon separated values (like CSV):
+
+````
+
+▒██   ██▒ ██▓ ███▄    █   ▄████ ▓█████▄  █    ██  ███▄ ▄███▓ ██▓███  ▓█████  ██▀███  
+▒▒ █ █ ▒░▓██▒ ██ ▀█   █  ██▒ ▀█▒▒██▀ ██▌ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒
+░░  █   ░▒██▒▓██  ▀█ ██▒▒██░▄▄▄░░██   █▌▓██  ▒██░▓██    ▓██░▓██░ ██▓▒▒███   ▓██ ░▄█ ▒
+ ░ █ █ ▒ ░██░▓██▒  ▐▌██▒░▓█  ██▓░▓█▄   ▌▓▓█  ░██░▒██    ▒██ ▒██▄█▓▒ ▒▒▓█  ▄ ▒██▀▀█▄  
+▒██▒ ▒██▒░██░▒██░   ▓██░░▒▓███▀▒░▒████▓ ▒▒█████▓ ▒██▒   ░██▒▒██▒ ░  ░░▒████▒░██▓ ▒██▒
+▒▒ ░ ░▓ ░░▓  ░ ▒░   ▒ ▒  ░▒   ▒  ▒▒▓  ▒ ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░▒▓▒░ ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░
+░░   ░▒ ░ ▒ ░░ ░░   ░ ▒░  ░   ░  ░ ▒  ▒ ░░▒░ ░ ░ ░  ░      ░░▒ ░      ░ ░  ░  ░▒ ░ ▒░
+ ░    ░   ▒ ░   ░   ░ ░ ░ ░   ░  ░ ░  ░  ░░░ ░ ░ ░      ░   ░░          ░     ░░   ░ 
+ ░    ░   ░           ░       ░    ░       ░            ░               ░  ░  by LRVT                                                   
+
+[i] Company Name: Apple
+[i] Company X-ID: 271572.8873b4
+[i] Company Slug: appleretaildeutschlandgmbh
+[i] Dumping Date: 24/12/2021 13:37:00
+
+Firstname;Lastname;Position;Gender;Location;E-Mail;Fax;Mobile;Phone;Profile
+Mina;Abdallah;RFIC Design Engineer;MALE;Unterhaching,Deutschland;None;None;None;None;https://www.xing.com/profile/Mina_Abdallah
+Isma;Abdan;Gabelstaplerfahrer;MALE;Huelva,Spanien;None;None;None;None;https://www.xing.com/profile/Isma_Abdan
+
+[i] Successfully crawled 2 Apple employees. Hurray ^_-
+````
---
+++ xingdumper.py
@@ -0,0 +1,119 @@
+import requests
+import json
+import argparse
+from datetime import datetime
+
+LOGIN_COOKIE = "<INSERT-YOUR-XING-LOGIN-COOKIE-VALUE>"
+
+parser = argparse.ArgumentParser("xingdumper.py")
+parser.add_argument("--url", metavar='<xing-url>', help="A XING company url - https://xing.com/pages/<company>", type=str, required=True)
+parser.add_argument("--count", metavar='<number>', help="Amount of employees to extract - max. 2999", type=int, required=False)
+parser.add_argument("--cookie", metavar='<cookie>', help="XING 'login' cookie for authentication", type=str, required=False,)
+parser.add_argument("--full", help="Dump additional contact details (slow) - email, phone, fax, mobile", required=False, action='store_true')
+parser.add_argument("--quiet", help="Show employee results only", required=False, action='store_true')
+
+args = parser.parse_args()
+url = args.url
+
+if (args.cookie):
+	LOGIN_COOKIE = args.cookie
+
+if (args.count and args.count < 3000):
+	count = args.count
+else:
+	# according to XING, the result window must be less than 3000
+	count = 2999
+
+api = "https://www.xing.com/xing-one/api"
+headers = {'Content-type': 'application/json'}
+cookies_dict = {"login": LOGIN_COOKIE}
+
+if (url.startswith('https://www.xing.com/pages/')):
+	try:
+		before_keyword, keyword, after_keyword = url.partition('pages/')
+		company = after_keyword
+
+		# retrieve company id from the api
+		postdata1 = {"operationName":"EntitySubpage","variables":{"id":company,"moduleType":"employees"},"query":"query EntitySubpage($id: SlugOrID!, ) {\n entityPageEX(id: $id) {\n ... on EntityPage {\n slug\n  title\n context {\n  companyId\n }\n  }\n }\n}\n"}
+		r = requests.post(api, data=json.dumps(postdata1), headers=headers, cookies=cookies_dict)
+		response1 = r.json()
+
+		companyID = response1["data"]["entityPageEX"]["context"]["companyId"]
+		
+		# retrieve employee information from the api based on previously obtained company id
+		postdata2 = {"operationName":"Employees","variables":{"consumer":"","id":companyID,"first":count,"query":{"consumer":"web.entity_pages.employees_subpage","sort":"CONNECTION_DEGREE"}},"query":"query Employees($id: SlugOrID!, $first: Int, $after: String, $query: CompanyEmployeesQueryInput!, $consumer: String! = \"\", $includeTotalQuery: Boolean = false) {\n  company(id: $id) {\n id\n totalEmployees: employees(first: 0, query: {consumer: $consumer}) @include(if: $includeTotalQuery) {\n total\n }\n employees(first: $first, after: $after, query: $query) {\n total\n edges {\n node {\n profileDetails {\n id\n firstName\n lastName\n displayName\n gender\n pageName\n location {\n displayLocation\n  }\n occupations {\n subline\n }\n }\n }\n }\n }\n }\n}\n"}
+		r2 = requests.post(api, data=json.dumps(postdata2), headers=headers, cookies=cookies_dict)
+		response2 = r2.json()
+
+		if not args.quiet:
+
+			print("""\
+
+▒██   ██▒ ██▓ ███▄    █   ▄████ ▓█████▄  █    ██  ███▄ ▄███▓ ██▓███  ▓█████  ██▀███  
+▒▒ █ █ ▒░▓██▒ ██ ▀█   █  ██▒ ▀█▒▒██▀ ██▌ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒
+░░  █   ░▒██▒▓██  ▀█ ██▒▒██░▄▄▄░░██   █▌▓██  ▒██░▓██    ▓██░▓██░ ██▓▒▒███   ▓██ ░▄█ ▒
+ ░ █ █ ▒ ░██░▓██▒  ▐▌██▒░▓█  ██▓░▓█▄   ▌▓▓█  ░██░▒██    ▒██ ▒██▄█▓▒ ▒▒▓█  ▄ ▒██▀▀█▄  
+▒██▒ ▒██▒░██░▒██░   ▓██░░▒▓███▀▒░▒████▓ ▒▒█████▓ ▒██▒   ░██▒▒██▒ ░  ░░▒████▒░██▓ ▒██▒
+▒▒ ░ ░▓ ░░▓  ░ ▒░   ▒ ▒  ░▒   ▒  ▒▒▓  ▒ ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░▒▓▒░ ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░
+░░   ░▒ ░ ▒ ░░ ░░   ░ ▒░  ░   ░  ░ ▒  ▒ ░░▒░ ░ ░ ░  ░      ░░▒ ░      ░ ░  ░  ░▒ ░ ▒░
+ ░    ░   ▒ ░   ░   ░ ░ ░ ░   ░  ░ ░  ░  ░░░ ░ ░ ░      ░   ░░          ░     ░░   ░ 
+ ░    ░   ░           ░       ░    ░       ░            ░               ░  ░  by LRVT
+			""")
+
+			print("[i] Company Name: " + response1["data"]["entityPageEX"]["title"])
+			print("[i] Company X-ID: " + companyID)
+			print("[i] Company Slug: " + company)
+			print("[i] Dumping Date: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+			print()
+
+		if args.full:
+			legende = "Firstname;Lastname;Position;Gender;Location;E-Mail;Fax;Mobile;Phone;Profile"
+		else:
+			legende = "Firstname;Lastname;Position;Gender;Location;Profile"
+		
+		print(legende)
+
+		dump_count = 0
+
+		# loop over employees
+		for employee in response2['data']['company']['employees']['edges']:
+			dump_count += 1
+			firstname = employee['node']['profileDetails']['firstName']
+			lastname = employee['node']['profileDetails']['lastName']
+			try:
+				position = employee['node']['profileDetails']['occupations'][0]['subline']
+			except:
+				position = "None"
+			gender = employee['node']['profileDetails']['gender']
+			location = employee['node']['profileDetails']['location']['displayLocation'].replace('**','').replace(', ',',')
+			pagename = employee['node']['profileDetails']['pageName']
+
+			if args.full:
+				# dump additional contact details for each employee. Most often is "None", so no default api queries for this data
+				postdata3 = {"operationName":"getXingId","variables":{"profileId":pagename},"query":"query getXingId($profileId: SlugOrID!, $actionsFilter: [AvailableAction!]) {\n  profileModules(id: $profileId) {\n    __typename\n    xingIdModule(actionsFilter: $actionsFilter) {\n      xingId {\n        status {\n          localizationValue\n          __typename\n        }\n        __typename\n      }\n      __typename\n      ...xingIdContactDetails\n       }\n  }\n}\n\nfragment xingIdContactDetails on XingIdModule {\n  contactDetails {\n    business {\n          email\n      fax {\n        phoneNumber\n   }\n      mobile {\n        phoneNumber\n  }\n      phone {\n        phoneNumber\n   }\n   }\n        __typename\n  }\n  __typename\n}\n"}
+				r3 = requests.post(api, data=json.dumps(postdata3), headers=headers, cookies=cookies_dict)
+				response3 = r3.json()
+				email = response3['data']['profileModules']['xingIdModule']['contactDetails']['business']['email']
+				fax = response3['data']['profileModules']['xingIdModule']['contactDetails']['business']['fax']
+				mobile = response3['data']['profileModules']['xingIdModule']['contactDetails']['business']['mobile']
+				phone = response3['data']['profileModules']['xingIdModule']['contactDetails']['business']['phone']
+				# print employee information as Comma Separated Values (CSV)
+				print(firstname + ";" + lastname + ";" + position + ";" + gender + ";" + location + ";" + str(email) + ";" + str(fax) + ";" + str(mobile) + ";" + str(phone) + ";" + "https://www.xing.com/profile/" + pagename)
+			else:
+				print(firstname + ";" + lastname + ";" + position + ";" + gender + ";" + location + ";" + "https://www.xing.com/profile/" + pagename)
+
+		if not args.quiet:
+			print()
+			print("[i] Successfully crawled " + str(dump_count) + " " + response1["data"]["entityPageEX"]["title"] + " employees. Hurray ^_-")
+	
+	except Exception as e:
+		# likely authorization error due to incorrect 'login' cookie
+		# otherwise the script is broken or the api has been changed
+		print()
+		print("[!] Authentication required.")
+		print("    > Set 'LOGIN_COOKIE' variable or use the '--cookie' CLI flag!")
+		#print("[debug] " + str(e))
+else:
+	print()
+	print("[!] Invalid URL provided.")
+	print("[i] Example URL: 'https://www.xing.com/pages/appleretaildeutschlandgmbh'")
